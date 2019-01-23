dc1a96333a837e65ccacc22e3181166f93b0af54
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-23 18:40:55 +0900

    Done
    done

--- 問題7/main.c
+++ 問題7/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+/*
+
+C言語認定試験向け課題　問7
+
+
+１．ｉｎｔ型変数ａにデータを１０進入力する
+２．ｉｎｔ型変数ｂにデータを１０進入力する
+３．ａとｂの内容を入れ替える
+４．１行目にａを５桁で１０進出力する
+５．２行目にｂを５桁で１０進出力する
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+    int a,b,work;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    work = a;
+    a=b;
+    b=work;
+    printf("%5d\n",a);
+    printf("%5d",b);
     return 0;
 }

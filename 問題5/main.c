cab68455e5a3779ad8ed2759f08d313f86f216d5
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-23 18:38:59 +0900

    Done
    done

--- 問題5/main.c
+++ 問題5/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+C言語認定試験向け課題　問5
+
+１．ｌｏｎｇ型変数ａにデータを１０進入力する
+２．ｌｏｎｇ型変数ｂにデータを１０進入力する
+３．１行目にａとbの和を８桁で１０進出力する
+４．２行目にａとbの差を８桁で１０進出力する
+５．３行目にａとbの積を８桁で１０進出力する
+６．４行目にａとbの商を８桁で１０進出力する
+７．５行目にａとbの剰余を８桁で１０進出力する
+
+
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+
+    long a,b;
+    scanf("%ld",&a);
+    scanf("%ld",&b);
+    printf("%8ld\n",a+b);
+    printf("%8ld\n",a-b);
+    printf("%8ld\n",a*b);
+    printf("%8ld\n",a/b);
+    printf("%8ld\n",a%b);
+
     return 0;
 }
--- 問題6/main.c
+++ 問題6/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+C言語認定試験向け課題　問6
+１．ｃｈａｒ型変数ａにデータを１文字入力する
+２．１行目にａを２桁で１６進出力する（大文字）
+３．２行目にａを文字出力する
+４．ｉｎｔ型変数ｂにａを代入する
+５．３行目にｂを２桁で１６進出力する
+６．４行目にｂを文字出力する
+*/
 int main()
 {
-    printf("Hello world!\n");
+    char a;
+    int b;
+    scanf("%c",&a);
+    printf("%2X\n",a);
+    printf("%c\n",a);
+    b=a;
+    printf("%2X\n",b);
+    printf("%c",b);
     return 0;
 }

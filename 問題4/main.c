5f97aeb84c9646111277c348c002b26dd2799378
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-23 18:20:21 +0900

    done
    done

--- 問題4/main.c
+++ 問題4/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+C言語認定試験向け課題　問4
+
+１．ｄｏｕｂｌｅ型変数ａにデータを入力する
+２．ｉｎｔ型変数ｂにａを代入する
+３．１行目にａを小数形式（小数点以下２桁）で出力する
+４．２行目にｂを１０進出力する
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+
+
+    double a;
+    int b;
+    scanf("%lf",&a);
+    b = a;
+    printf("%.2f\n",a);
+    printf("%d",b);
+
     return 0;
 }

00d6ea014439dfceb48d18f6e67c3db1259a47d1
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-23 21:32:35 +0900

    Done
    Done

--- 問題15/main.c
+++ 問題15/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+CŒ¾Œê”F’èŒ±Œü‚¯‰Û‘è@–â15
+
+‚PD‚‰‚‚”Œ^•Ï”‚‚Éf‚`f‚ğ‘ã“ü‚·‚é
+‚QD‚‚ğ•¶šo—Í‚·‚é
+‚RD‚‚É‚P‚ğ‘«‚µ‚Ş
+‚SD‚‚ªf‚yfˆÈ‰º‚Å‚ ‚ê‚Î‚QD‚Ö–ß‚é
+https://github.com/jmmedel/C_kadai
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+
+
+     int a,b;
+
+    a='A';
+    while(a<='Z')
+    {
+        printf("%c",a);
+        a++;
+
+    }
+
     return 0;
 }
--- 問題16/main.c
+++ 問題16/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+
+    int a= 'z',i=0;
+while(a>='a')
+{
+    printf("%c",a);
+    i++;
+    a=1;
+    if(i%7==0){
+        printf("\n");
+    }
+
+}
+
 }

a554237246c5f8094d8de38160c743a4c3d29a33
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-23 20:39:11 +0900

    done
    done

--- 問題13/main.c
+++ 問題13/main.c
@@ -2,10 +2,25 @@
 #include <stdlib.h>
 
 
+/*
 
+C言語認定試験向け課題　問13
+
+１．ｆｌｏａｔ型変数ａにデータを入力する
+２．１行目にａを小数形式（小数点以下２桁）で出力する
+３．２行目にａを指数形式（小数点以下２桁）で出力する
+（小文字）
+https://github.com/jmmedel/C_kadai
+*/
 
 int main()
 {
-    printf("Hello world!\n");
+
+    float a;
+    scanf("%f",&a);
+    printf("%.2f\n",a);
+    printf("%.2e\n",a);
+
+
     return 0;
 }
--- 問題14/main.c
+++ 問題14/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+C言語認定試験向け課題　問14
+
+１．ｉｎｔ型変数ａにデータを１０進入力する
+２．ａが０以下か２１以上の間１．を繰り返す
+３．１行目からａ行目まで’*’をａ個並べて出力する
+https://github.com/jmmedel/C_kadai
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+
+
+    int a,i,j;
+    scanf("%d",&a);
+    while(a<=0 || a>=21)
+    scanf("%d",&a);
+    for(i=0;i<a;i++){
+
+    for(j=0;j<a;j++)
+        printf("*");
+    printf("\n");
+    }
+
+
     return 0;
 }

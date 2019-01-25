e92ddd72a88347616ff3f5c7cf0449bf33afa7a9
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-25 20:40:28 +0900

    done
    done

--- 問題17/main.c
+++ 問題17/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+C言語認定試験向け課題　問17
+
+１．ｉｎｔ型変数ａにデータを１０進入力する
+２．ａが１であれば”ｖｉｎ”を出力する
+３．ａが２であれば”ｈｅｌｐａｓ　ｖｉｎ”を出力する
+４．ａが３であれば”ｎｅ　ｈｅｌｐａｓ　ｖｉｎ”を出力する
+５．ａが４であれば”Ｍｉ　ｎｅ　ｈｅｌｐａｓ　ｖｉｎ”を出力する
+６．ａが１から４以外であれば”ｅｒｒｏｒ”を出力する
+
+ */
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+
+    int a;
+    scanf("%d",&a);
+    switch(a){
+
+    case 4:
+        printf("Mi");
+    case 3:
+        printf("ne");
+    case 2:
+        printf("helpas");
+    case 1:
+        printf("Vin");
+        break;
+    default:
+        printf("Error");
+
+}
+
 }

71619dee2b0bd5ccb00aad9808fe55c4ade9c629
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-25 21:55:13 +0900

    Done
    Dinbe

--- 問題20/main.c
+++ 問題20/main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+
+/*
+
+C言語認定試験向け課題　問20
+
+１．ｄｏｕｂｌｅ型１次元配列ａにデータを入力する
+２．ＥＯＦでない間１．を繰り返す（最大１００回）
+３．ａの各要素を、入力されたデータだけ、小数形式１０桁（小数点以下２桁）で出力する
+４．データ４個ごとに改行する
+
+*/
+
+void main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+
+double a[100],work;
+int i,j;
+
+for(i=0; i<100; i++)
+{
+    if(scanf("%lf",&work)==EOF)
+        break;
+    else
+        a[i]=work;
+
+}
+
+for(j=0;j<i;j++)
+{
+    if(j%4==0){
+        printf("\n");
+    }
+
+    printf("%10.2f",a[j]);
+
+}
+
 }

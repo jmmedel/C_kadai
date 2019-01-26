15c800672472abe7ce7fc8fcc771c97a7768c88b
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-26 09:11:58 +0900

    done
    done

--- 問題25/main.c
+++ 問題25/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+/*
+
+
+CŒ¾Œê”F’èŽŽŒ±Œü‚¯‰Û‘è@–â25
+
+‚PD‚‰‚Ž‚”Œ^•Ï”‚‚Éƒf[ƒ^‚ð‚P‚Oi“ü—Í‚·‚é
+‚QD‚‚ð‚P‚UŒ…‚Å‚Qio—Í‚·‚é
+
+*/
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+    int a,i;
+    scanf("%d",&a);
+    for(i =15,i>=0;i--;)
+    {
+    printf("%d",((a>>i)&0X0001));
+
+    }
+
 }

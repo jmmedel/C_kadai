82a4f26f3b1dede8db99b554466bdceaee52a090
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-01-27 00:32:45 +0900

    Done
    Done

--- 問題33/main.c
+++ 問題33/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+
+/*
+
+C言語認定試験向け課題　問33
+
+main（）関数を使わずにcプログラムを書くことができます。
+そのためには、＃defineinプリプロセッサディレクティブを
+使用する必要があります。
+
+*/
+#define start main
+void start() {
+
+   printf("Hello");
+
 }
--- 問題34/main.c
+++ 問題34/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+
+/*
+
+C言語認定試験向け課題　問34
+Cでの10進数から2進数へ：cプログラムによって、
+任意の10進数（base  -  10（0から9））
+を2進数（base  -  2（0または1））に変換できます。
+
+*/
+int main(){
+
+int a[10],n,i;
+
+printf("変換する番号を入力してください。");
+scanf("%d",&n);
+for(i=0;n>0;i++)
 {
-    printf("Hello world!\n");
-    return 0;
+a[i]=n%2;
+n=n/2;
+}
+printf("\n与えられた数のバイナリは=");
+for(i=i-1;i>=0;i--)
+{
+printf("%d",a[i]);
+}
+return 0;
+
 }
